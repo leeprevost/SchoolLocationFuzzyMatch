9a75ca16cb41e06922fdf299da699b079d02eb81
leeprevost <lee@prevost.net>
2018-04-19 09:23:07 -0400

    tried various feature tweaks

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.feature_selection import SelectPercentile
+from sklearn.multiclass import OneVsRestClassifier
 
 from sklearn.pipeline import FeatureUnion, Pipeline
 
@@ -17,6 +18,7 @@ pipeline = Pipeline([
             ('extract', ColumnExtractor('norm_SCH_NAME')),
             ('vectorize', CountVectorizer(ngram_range=(1, 2))),
             ('select', SelectPercentile(percentile=5)),
+            #('ovr', OneVsRestClassifier(estimator = LinearSVC(random_state=0))),
             #('classify', ModelTransformer(SGDClassifier(
              #   loss='modified_huber')))
             #('classify', LinearSVC())
@@ -49,5 +51,5 @@ pipeline = Pipeline([
             #('is_weekend', IsWeekendTransformer())
         #])),
     ])),
-    ('classifier', LinearSVC())
+    ('classifier', OneVsRestClassifier(LinearSVC()))
 ])
